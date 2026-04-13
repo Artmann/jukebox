209d79b3bbe7b5f3d2270405cafe00c1f722d7d8
Christoffer Artmann <artgaard@gmail.com>
2026-04-13 10:17:18 +0200

    Add Continue Watching, genre categories, and loading states

--- src/app/pages/Movies.tsx
+++ src/app/pages/Movies.tsx
@@ -1,14 +1,20 @@
 import { useMovies } from '../hooks/useMovies'
 import { LibraryGrid } from '../components/LibraryGrid'
 import { PageHeader } from '../components/PageHeader'
+import { SkeletonGrid } from '../components/SkeletonGrid'
 
 export function MoviesPage() {
-  const { data: movies } = useMovies()
+  const { data: movies, isLoading } = useMovies()
 
   return (
     <>
       <PageHeader />
-      <LibraryGrid items={movies || []} />
+
+      {isLoading ? (
+        <SkeletonGrid />
+      ) : (
+        <LibraryGrid items={movies ?? []} />
+      )}
     </>
   )
 }
